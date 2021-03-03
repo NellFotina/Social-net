7ec290164f116cbfb484586935d054187428c496
NellFotina <fotina.nel@gmail.com>
2021-03-03 13:36:42 +0200

    lesson 11

---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+const Profile = () => {
+return <main className="main">
+<div>
+  <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" />
+</div>
+<div>ava + description</div>
+<div>
+  My posts
+  <div>
+    New post
+    </div>
+    <div>post 1</div>
+    <div>post 2</div>
+</div>
+</main>
+}
+
+export default Profile;
