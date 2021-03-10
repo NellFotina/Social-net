18edf5917b6a1227fe5cfdbe2e55e9050d2eb415
NellFotina <fotina.nel@gmail.com>
2021-03-10 13:10:59 +0200

    lesson 21 (verstka page Messages /dialogs/

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-import s from './Dialogs.module.css';
+import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
-    return (
-        <div>
-            Dialogs
-        </div>
-    )
-}
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + " " + s.active}>Dima</div>
+        <div className={s.dialog}>Nel</div>
+        <div className={s.dialog}>Yura</div>
+        <div className={s.dialog}>Oleg</div>
+        <div className={s.dialog}>Natasha</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>How are you</div>
+        <div className={s.message}>Yo</div>
+      </div>
+    </div>
+  );
+};
 
 export default Dialogs;
