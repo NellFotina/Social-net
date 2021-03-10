98d311e45c162854fe78086685dd10fda8dac743
NellFotina <fotina.nel@gmail.com>
2021-03-10 12:10:05 +0200

    lesson 19

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,26 @@
 import React from "react";
-import s from './Navbar.module.css';
-
-console.log(s);
-
-// let s {
-//   'nav': 'Navbar_nav__1QIal',
-//   'item': 'Navbar_item__1iKA-',
-//   'active': 'Navbar_active__3CcyT'
-// }
-
-let s1 = "item";
-let s2 = "active";
-// "item active"
-let classes = s1 + " " + s2;
-let classesNew = `${s.item} ${s.active}`;
+import s from "./Navbar.module.css";
 
 const Navbar = () => {
-return <nav className={s.nav}>
-<div className={`${s.item} ${s.active}`}>
-  <a>Profile</a>
-</div>
-<div className={s.item}>
-  <a>Messages</a>
-</div>
-<div className={s.item}>
-  <a>News</a>
-</div>
-<div className={s.item}>
-  <a>Music</a>
-</div>
-<div className={s.item}>
-  <a>Settings</a>
-</div>
-</nav>
-}
+  return (
+    <nav className={s.nav}>
+      <div className={`${s.item} ${s.active}`}>
+        <a href="/profile">Profile</a>
+      </div>
+      <div className={s.item}>
+        <a href="/dialogs">Messages</a>
+      </div>
+      <div className={s.item}>
+        <a href="/news">News</a>
+      </div>
+      <div className={s.item}>
+        <a href="/music">Music</a>
+      </div>
+      <div className={s.item}>
+        <a href="/settings">Settings</a>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
