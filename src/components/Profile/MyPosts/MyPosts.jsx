190e0701579fe61c515b78016803394e7eaf2e88
NellFotina <fotina.nel@gmail.com>
2021-03-10 20:37:57 +0200

    lesson 26 (UI - BLL)

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,14 +2,9 @@ import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
-const MyPosts = () => {
+const MyPosts = (props) => {
 
-let posts = [
-  {id: 1, message: "Hi, how are you", likeCount: 5},
-  {id: 2, message: "It's my first post", likeCount: 15}
-]
-
-let postsElements = posts.map (p => <Post message={p.message} like={p.likeCount} />);
+let postsElements = props.posts.map (p => <Post message={p.message} like={p.likeCount} />);
 
   return (
     <div className={s.postBlock}>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,11 +3,16 @@ import s from "./Profile.module.css";
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
+let posts = [
+  {id: 1, message: "Hi, how are you", likeCount: 5},
+  {id: 2, message: "It's my first post", likeCount: 15}
+]
+
 const Profile = () => {
   return (
     <main>
       <ProfileInfo />
-      <MyPosts />
+      <MyPosts posts={posts} />
     </main>
   );
 };
