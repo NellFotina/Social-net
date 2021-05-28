cb29ad8b8ad79074a7dac093424e76487589f54f
NellFotina <fotina.nel@gmail.com>
2021-05-28 10:54:04 +0300

    lesson 54. Жизненный цикл. life cycle method, componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,8 +4,7 @@ import styles from "./Users.module.css";
 import userPhoto from "../../assets/image/user_photo.png";
 
 class Users extends React.Component {
-  constructor(props) {
-    super(props);
+  componentDidMount() {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
       .then((response) => {
