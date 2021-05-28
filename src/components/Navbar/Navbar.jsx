cc2e97e88934cadd935ab1415b6761de6de4c3ba
NellFotina <fotina.nel@gmail.com>
2021-05-28 09:20:06 +0300

    lesson 53 - классовый компонент. extend React.Component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,27 +4,49 @@ import Friends from "../Friends/Friends";
 import s from "./Navbar.module.css";
 
 const Navbar = (props) => {
- 
   return (
     <nav className={s.nav}>
       <div className={s.item}>
-        <NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>
+        <NavLink to="/profile" activeClassName={s.active}>
+          Profile
+        </NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to="/dialogs" activeClassName={s.active}>Messages</NavLink>
+        <NavLink to="/dialogs" activeClassName={s.active}>
+          Messages
+        </NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to="/news" activeClassName={s.active}>News</NavLink>
+        <NavLink to="/users" activeClassName={s.active}>
+          Users
+        </NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to="/music" activeClassName={s.active}>Music</NavLink>
+        <NavLink to="/news" activeClassName={s.active}>
+          News
+        </NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to="/settings" activeClassName={s.active}>Settings</NavLink>
+        <NavLink to="/music" activeClassName={s.active}>
+          Music
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/settings" activeClassName={s.active}>
+          Settings
+        </NavLink>
       </div>
       <div className={s.item + " " + s.friends}>
-        <NavLink to="/friends" className={s.navFriends} activeClassName={s.active}>Friends</NavLink>
-        <div><Friends state={props.state}/></div>
+        <NavLink
+          to="/friends"
+          className={s.navFriends}
+          activeClassName={s.active}
+        >
+          Friends
+        </NavLink>
+        <div>
+          <Friends state={props.state} />
+        </div>
       </div>
     </nav>
   );
